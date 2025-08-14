ca41457a8cc5413a96e6db92d32a2f690315ef67
Huzaifa Bin Habib <huzaifabinhabib12577@gmail.com>
2025-08-15 03:48:19 +0500

    Time: 0 ms (100%), Space: 13.8 MB (15.78%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int compress(vector<char>& a) {
+        vector<char> ans;
+        int count = 1;
+        for (int i = 0; i < a.size(); i++) {
+            if (i + 1 < a.size() && a[i] == a[i + 1]) {
+                count++;
+            } else {
+                ans.push_back(a[i]);                 
+                if (count > 1) {
+                    string s = to_string(count); 
+                    for (char val : s){ 
+                        ans.push_back(val);
+                        };
+                }
+                count = 1; 
+            }
+        }
+        a = ans; 
+        return a.size();
+    }
+};
