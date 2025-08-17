61713271c09d6af485609283fc6d27993686c59c
Huzaifa Bin Habib <huzaifabinhabib12577@gmail.com>
2025-08-17 22:58:46 +0500

    Time: 0 ms (100%), Space: 9.3 MB (49.49%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& m) {
+        int srows = 0;
+        int erows = m.size() - 1;
+
+        int scols = 0;
+        int ecols = m[0].size() - 1;
+
+        vector<int> ans;
+
+        while (srows <= erows && scols <= ecols) {
+
+            for (int i = scols; i <= ecols; i++) {
+                ans.push_back(m[scols][i]);
+            }
+            for (int i = srows + 1; i <= erows - 1; i++) {
+                ans.push_back(m[i][ecols]);
+            }
+            for (int i = ecols; i >= scols; i--) {
+                if (erows == srows) {
+                    break;
+                }
+                ans.push_back(m[erows][i]);
+            }
+            for (int i = erows - 1; i >= srows + 1; i--) {
+                if (ecols == scols) {
+                    break;
+                }
+                ans.push_back(m[i][scols]);
+            }
+            scols++;
+            ecols--;
+            srows++;
+            erows--;
+        }
+
+        return ans;
+    }
+};
