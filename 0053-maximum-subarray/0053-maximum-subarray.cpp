f0434503e47720aa697c2aa0e66ef2ac7cdefbfd
Huzaifa Bin Habib <huzaifabinhabib12577@gmail.com>
2025-10-28 18:50:17 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curr=0;
-        int maxnum= INT_MIN;
-        for(int i=0; i<nums.size();i++){
-             curr += nums[i];
-             maxnum = max(curr,maxnum);
-             if(curr<0){
-                curr=0;
-             }
+        int currsum=0;
+        int maxsum =0;
+        for(int i=0;i< nums.size();i++){
+            currsum += nums[i];
+             maxsum = max(currsum,maxsum);
+            if(currsum <= 0){
+                currsum = 0;
+            }
         }
-        return maxnum;
+        return maxsum;
     }
 };
