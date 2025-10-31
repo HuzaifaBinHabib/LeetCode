3fc9e6a2e201b54ab8eab8dc6229d996639fd950
Huzaifa Bin Habib <huzaifabinhabib12577@gmail.com>
2025-10-31 19:48:16 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& arr) {
-      int lp=0;
-    int n= arr.size();
-    int rp=n-1;
-    if(n==1){return arr[0];}
+        int n = arr.size();
+        int lp = 0;
+        int rp = n - 1;
+        if(n==1){
+            return arr[0];
+        }
+        while (lp <= rp) {
+            int mid = lp + (rp - lp) / 2;
 
-    while(lp<=rp){
-        int mid = lp+(rp-lp)/2;
-        
-    if (mid == 0 && arr[mid] != arr[mid + 1]){
-        return arr[mid];
-        }
-    if(mid==n-1 && arr[mid] != arr[n-1]) {
-        return arr[mid];
-        }
-    if (arr[mid-1] != arr[mid] && arr[mid] != arr[mid+1]) {
-        return arr[mid];
-        }
-        
-        if(mid%2==0){
-            if(arr[mid]==arr[mid-1]){
-                rp= mid-1;
-            }else{
-                lp=mid+1;
+            if (arr[mid - 1] != arr[mid] && arr[mid + 1] != arr[mid]) {
+                return arr[mid];
             }
-        }else{
-            if(arr[mid]==arr[mid-1]){
-                lp=mid+1;
-            }else{
-                rp=mid-1;
+            if (mid % 2 == 0) {
+                if (arr[mid] == arr[mid - 1]) {
+                    rp = mid - 1;
+                } else {
+                    lp = mid + 1;
+                }
+            } else {
+                if (arr[mid] == arr[mid - 1]) {
+                    lp = mid + 1;
+                } else {
+                    rp = mid - 1;
+                }
             }
         }
+        return -1;
     }
-    return -1;
-}
-    
 };
