59b7de8e65a0e5f51930deb28ec18e5d7c8a2966
Huzaifa Bin Habib <huzaifabinhabib12577@gmail.com>
2025-11-05 22:12:01 +0500

    Time: 0 ms (100%), Space: 13.4 MB (42.17%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-        bool searchMatrix(vector<vector<int>>& m, int target) {
-        int l1=m.size();
-        int l2=m[0].size();
-        for(int i=0;i<l1;i++){
-        int lp=0;
-        int rp =l2-1;
-           while(lp<=rp){
-             if(m[i][lp]==target || m[i][rp]==target){
-                 return true;
-             }
-             lp++;
-             rp--;
-             
-           }
+    bool search(vector<vector<int>>& mat, int target, int row, int col) {
+        int lp = 0, rp = col - 1;
+        while (lp <= rp) {
+            int midcol = lp + (rp - lp) / 2;
+            if (target == mat[row][midcol]) {
+                return true;
+            } else if (target > mat[row][midcol]) {
+                lp = midcol + 1;
+            } else {
+                rp = midcol - 1;
+            }
         }
-        return false; 
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int r = mat.size(); int c = mat[0].size();
+        int lp = 0;
+        int rp = r - 1;
+
+        while (lp <= rp) {
+            int midrow = lp + (rp - lp) / 2;
+
+            if (target >= mat[midrow][0] && target <= mat[midrow][c - 1]) {
+                return search(mat, target, midrow, c);
+            } else if (target >= mat[midrow][c - 1]) {
+                lp = midrow + 1;
+            } else {
+                rp = midrow - 1;
+            }
+        }
+        return false;
     }
 };
