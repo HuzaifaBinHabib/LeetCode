c173bc3111ce2f9a5569b03b32793030c9463b53
Huzaifa Bin Habib <huzaifabinhabib12577@gmail.com>
2025-11-07 18:20:59 +0500

    Time: 19 ms (74.7%), Space: 17.3 MB (98.4%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
+    vector<vector<int>> fourSum(vector<int>& nums, int tar) {
         int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
+            }
+
             for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1])
+                if (j > i+1 && nums[j] == nums[j - 1]) {
                     continue;
+                }
                 int lp = j + 1;
                 int rp = n - 1;
                 while (lp < rp) {
-                   long long int sum = nums[i] + nums[j] + nums[lp] + nums[rp];
-                    if (sum < target) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[lp] + nums[rp];
+                    if (sum < tar) {
                         lp++;
-                    } else if (sum > target) {
+                    } else if (sum > tar) {
                         rp--;
                     } else {
                         ans.push_back({nums[i], nums[j], nums[lp], nums[rp]});
-                        lp++;
-                        rp--;
+                        lp++, rp--;
                         while (lp < rp && nums[lp] == nums[lp - 1]) {
                             lp++;
                         }
